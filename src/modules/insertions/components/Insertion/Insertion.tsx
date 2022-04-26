aa25ad5eb803e5bcf45ea42334717dabece59340
Alexey Selivanov <lazypacman@gmail.com>
2022-04-26 09:42:33 +0300

    Fix

--- src/modules/insertions/components/Insertion/Insertion.tsx
+++ src/modules/insertions/components/Insertion/Insertion.tsx
@@ -96,21 +96,4 @@ export const Insertion: FC<InsertionProps> = ({ children, ...props }) => {
       </div>
     )
   }
-
-  /*
-  if (!props.children?.[0] || !props.type) {
-    return null
-  }
-
-  let dir = props.children[0]
-  let image = props.type
-
-  return (
-    <div className={css.root}>
-      <img src={`/images/${dir}/${image}.svg`} />
-      {/!*<Image src={} width={100} height={100} />*!/}
-      {/!* <p>Insertion of type "{props.children}" not found</p> *!/}
-    </div>
-  )
-*/
 }
