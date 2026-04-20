b3ecb4d40977e902bad3884f95e650ed595ad168
Alexey Selivanov <lazypacman@gmail.com>
2026-04-20 13:15:35 +0400

    auto-save 2026-04-20T09:15:34.979Z

--- src/App.tsx
+++ src/App.tsx
@@ -24,10 +24,16 @@ const assetUrls: TldrawProps["assetUrls"] = {
     // tldraw_sans_italic: "/YsabeauOffice-Regular.ttf",
     // tldraw_sans_italic_bold: "/YsabeauOffice-Regular.ttf",
     //
-    tldraw_mono: "/fonts/Anonymous Pro Minus.ttf",
-    tldraw_mono_italic: "/fonts/Anonymous Pro Minus I.ttf",
-    tldraw_mono_bold: "/fonts/Anonymous Pro Minus B.ttf",
-    tldraw_mono_italic_bold: "/fonts/Anonymous Pro Minus BI.ttf",
+    //
+    tldraw_mono: "/fonts/fantasquesansmono-regular.otf",
+    tldraw_mono_italic: "/fonts/fantasquesansmono-italic.otf",
+    tldraw_mono_bold: "/fonts/fantasquesansmono-bold.otf",
+    tldraw_mono_italic_bold: "/fonts/fantasquesansmono-bolditalic.otf",
+
+    // tldraw_mono: "/fonts/Anonymous Pro Minus.ttf",
+    // tldraw_mono_italic: "/fonts/Anonymous Pro Minus I.ttf",
+    // tldraw_mono_bold: "/fonts/Anonymous Pro Minus B.ttf",
+    // tldraw_mono_italic_bold: "/fonts/Anonymous Pro Minus BI.ttf",
   },
 };
 
