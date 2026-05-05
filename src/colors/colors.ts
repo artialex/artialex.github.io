564fa1a6036a61ea2969402bdd5d70192b27e7f1
Alexey Selivanov <lazypacman@gmail.com>
2026-05-05 12:02:24 +0400

    auto-save 2026-05-05T08:02:23.976Z

--- src/colors/colors.ts
+++ src/colors/colors.ts
@@ -1,16 +1,29 @@
 import { DefaultColorThemePalette, defaultColorNames } from "tldraw";
 
+type TLColor = (typeof defaultColorNames)[number];
+
 // Dark Theme
-const colors: Record<(typeof defaultColorNames)[number], string> = {
+const colors: Record<TLColor, string> = {
   "light-violet": "oklch(74% 0.238 322.16)",
   violet: "oklch(71.4% 0.203 305.504)",
+  blue: "oklch(68.5% 0.169 237.323)",
 };
 
-for (const [key, color] of Object.entries(colors)) {
-  console.log(key, color);
+const style = document.createElement("style");
 
-  DefaultColorThemePalette.darkMode[key].solid = color;
+for (const [key, color] of Object.entries(colors)) {
+  DefaultColorThemePalette.darkMode[key as TLColor].solid = color;
+  style.textContent = `
+    :where([style*="color: ${color}"], [stroke="${color}"]) {
+      --base-color: white;
+      --primary-color: ${color};
+
+      stroke: var(--primary-color)
+    }
+  `;
 }
+
+document.head.appendChild(style);
 
 // DefaultColorThemePalette.darkMode["light-violet"].solid = "violet";
 // DefaultColorThemePalette.darkMode.violet.solid = "hotpink";
