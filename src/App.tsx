81514241494538aecbb7940b209aea70c24bc063
Alexey Selivanov <lazypacman@gmail.com>
2026-05-05 11:27:24 +0400

    auto-save 2026-05-05T07:27:23.945Z

--- src/App.tsx
+++ src/App.tsx
@@ -29,10 +29,10 @@ const assetUrls: TldrawProps["assetUrls"] = {
     // tldraw_sans_italic_bold: "/YsabeauOffice-Regular.ttf",
     //
     //
-    tldraw_mono: "/fonts/fantasquesansmono-regular.otf",
-    tldraw_mono_italic: "/fonts/fantasquesansmono-italic.otf",
-    tldraw_mono_bold: "/fonts/fantasquesansmono-bold.otf",
-    tldraw_mono_italic_bold: "/fonts/fantasquesansmono-bolditalic.otf",
+    // tldraw_mono: "/fonts/fantasquesansmono-regular.otf",
+    // tldraw_mono_italic: "/fonts/fantasquesansmono-italic.otf",
+    // tldraw_mono_bold: "/fonts/fantasquesansmono-bold.otf",
+    // tldraw_mono_italic_bold: "/fonts/fantasquesansmono-bolditalic.otf",
 
     // tldraw_mono: "/fonts/Anonymous Pro Minus.ttf",
     // tldraw_mono_italic: "/fonts/Anonymous Pro Minus I.ttf",
