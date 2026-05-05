c459356116d4513276a8e9839689f60f1dd31733
Alexey Selivanov <lazypacman@gmail.com>
2026-05-05 13:44:18 +0400

    auto-save 2026-05-05T09:44:18.092Z

--- src/colors/colors.ts
+++ src/colors/colors.ts
@@ -37,22 +37,6 @@ for (const [key, color] of Object.entries(colors)) {
 
 document.head.appendChild(style);
 
-// DefaultColorThemePalette.darkMode["light-violet"].solid = "violet";
-// DefaultColorThemePalette.darkMode.violet.solid = "hotpink";
-
-// DefaultColorThemePalette.darkMode.blue.solid = "dodgerblue";
-// DefaultColorThemePalette.darkMode["light-blue"].solid = "deepskyblue";
-// DefaultColorThemePalette.darkMode.yellow.solid = "gold";
-// DefaultColorThemePalette.darkMode.orange.solid = "darkorange";
-
-// DefaultColorThemePalette.darkMode.green.solid = "green";
-// DefaultColorThemePalette.darkMode["light-green"].solid = "mediumseagreen";
-
-// DefaultColorThemePalette.darkMode["light-red"].solid = "crimson";
-// DefaultColorThemePalette.darkMode.red.solid = "lightsalmon";
-
-// DefaultColorThemePalette.darkMode.grey.solid = "#64748b";
-
 // Light Theme
 
 DefaultColorThemePalette.lightMode.grey.solid = "darkgray";
