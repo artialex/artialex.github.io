b87bc0f468af5adf32af61e07ee4a53b76250d04
Alexey Selivanov <lazypacman@gmail.com>
2026-05-05 12:07:24 +0400

    auto-save 2026-05-05T08:07:24.020Z

--- src/colors/colors.ts
+++ src/colors/colors.ts
@@ -4,16 +4,25 @@ type TLColor = (typeof defaultColorNames)[number];
 
 // Dark Theme
 const colors: Record<TLColor, string> = {
-  "light-violet": "oklch(74% 0.238 322.16)",
-  violet: "oklch(71.4% 0.203 305.504)",
-  blue: "oklch(68.5% 0.169 237.323)",
+  "light-violet": "oklch(0.74 0.238 322.16)",
+  violet: "oklch(0.714 0.203 305.504)",
+
+  blue: "oklch(0.685 0.169 237.323)",
+  "light-blue": "oklch(0.789 0.154 211.53)",
+  yellow: "oklch(0.828 0.189 84.429)",
+  orange: "oklch(0.75 0.183 55.934)",
+
+  green: "oklch(0.6 0.118 184.704)",
+  "light-green": "oklch(0.696 0.17 162.48)",
+  "light-red": "oklch(0.64044 0.18091 14.083)",
+  red: "oklch(0.55732 0.20501 26.411)",
 };
 
 const style = document.createElement("style");
 
 for (const [key, color] of Object.entries(colors)) {
   DefaultColorThemePalette.darkMode[key as TLColor].solid = color;
-  style.textContent = `
+  style.textContent += `
     :where([style*="color: ${color}"], [stroke="${color}"]) {
       --base-color: white;
       --primary-color: ${color};
