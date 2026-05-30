eb2a777aa3411746636b23dcb7cbf09845152b1e
Alexey Selivanov <lazypacman@gmail.com>
2026-05-31 01:08:24 +0400

    auto-save 2026-05-30T21:08:24.790Z

--- src/modules/blocks/block-mono.ts
+++ src/modules/blocks/block-mono.ts
@@ -3,24 +3,12 @@ import type { TldrawProps } from 'tldraw';
 export const mono: TldrawProps = {
   assetUrls: {
     fonts: {
-      // tldraw_mono: '/fonts/Anonymous Pro Minus.ttf',
-      // tldraw_mono_italic: '/fonts/Anonymous Pro Minus I.ttf',
-      // tldraw_mono_bold: '/fonts/Anonymous Pro Minus B.ttf',
-      // tldraw_mono_italic_bold: '/fonts/Anonymous Pro Minus BI.ttf',
-      // tldraw_mono: '/fonts/GeistMono-Regular.ttf',
-      // tldraw_mono_italic: '/fonts/GeistMono-Regular.ttf',
-      // tldraw_mono_bold: '/fonts/GeistMono-Medium.ttf',
-      // tldraw_mono_italic_bold: '/fonts/GeistMono-Medium.ttf',
-
-      // tldraw_mono: '/fonts/ShantellSans-Regular.ttf',
-      // tldraw_mono_italic: '/fonts/ShantellSans-Italic.ttf',
-      // tldraw_mono_bold: '/fonts/ShantellSans-Medium.ttf',
-      // tldraw_mono_italic_bold: '/fonts/ShantellSans-MediumItalic.ttf',
-
       tldraw_mono: '/fonts/mononoki-Regular.ttf',
       tldraw_mono_italic: '/fonts/mononoki-Italic.ttf',
-      tldraw_mono_bold: '/fonts/mononoki-Medium.ttf',
-      tldraw_mono_italic_bold: '/fonts/mononoki-MediumItalic.ttf',
+      tldraw_mono_bold: '/fonts/mononoki-Bold.ttf',
+      tldraw_mono_italic_bold: '/fonts/mononoki-BoldItalic.ttf',
     },
   },
 };
+
+// Alternative fonts: Anonymous Pro, Geist Mono, JetBrains Mono
