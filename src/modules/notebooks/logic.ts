745450c66300d2f7d1323e6f4930f41c2f903d4f
Alexey Selivanov <lazypacman@gmail.com>
2026-06-02 15:48:03 +0400

    auto-save 2026-06-02T11:48:03.066Z

--- src/modules/notebooks/logic.ts
+++ src/modules/notebooks/logic.ts
@@ -7,7 +7,7 @@ export const notebooks: Record<string, string> = {
   _management: '🌵 Management',
 
   _javascript: 'JavaScript',
-  _typescript: 'TypeScript',
+  _typescript: '🪻 TypeScript',
   _golang: 'Golang',
   '_operating-systems': '🌿 Operating Systems',
   '_algorithms-n-data-structures': 'Algorithms & Data Structures',
