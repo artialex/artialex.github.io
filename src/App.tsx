d6d338f3f1f4bbc7df7c52eeac0b882cb44c0944
Alexey Selivanov <lazypacman@gmail.com>
2026-06-03 18:13:36 +0400

    auto-save 2026-06-03T14:13:36.326Z

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,6 @@ const assetUrls: TldrawProps['assetUrls'] = {
     tldraw_draw: withBase('fonts/MorningBreeze-Light.otf'),
     tldraw_draw_italic: withBase('fonts/MorningBreeze-Light.otf'),
     tldraw_draw_bold: withBase('fonts/PlaypenSans-Bold.ttf'),
-    // tldraw_draw_bold: withBase('fonts/MorningBreeze-Light.otf'),
-    // tldraw_draw_italic_bold: withBase('fonts/MorningBreeze-Light.otf'),
 
     ...mono.assetUrls?.fonts,
   },
