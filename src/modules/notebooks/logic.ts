6bbe56685032606c5320ea1e2c817508b38f3551
Alexey Selivanov <lazypacman@gmail.com>
2026-06-03 21:30:12 +0400

    auto-save 2026-06-03T17:30:11.982Z

--- src/modules/notebooks/logic.ts
+++ src/modules/notebooks/logic.ts
@@ -29,6 +29,7 @@ export const notebooks: Record<string, string> = {
   _algebra: '📘 Algebra',
   _trigonometry: 'Trigonometry',
   _combinatorics: 'Combinatorics',
+  _probability: 'Probability',
 
   // STEM
   _chemistry: '📗 Chemistry',
