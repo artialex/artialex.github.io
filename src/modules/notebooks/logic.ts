267d0a0fe965e5d34bd241f453d92b0391daf8cb
Alexey Selivanov <lazypacman@gmail.com>
2026-06-09 12:07:08 +0400

    auto-save 2026-06-09T08:07:08.872Z

--- src/modules/notebooks/logic.ts
+++ src/modules/notebooks/logic.ts
@@ -26,7 +26,7 @@ export const notebooks: Record<string, string> = {
   _swe: 'Software Engineering',
   _gamedev: 'Game Development',
   _backend: 'Backend',
-  _git: 'Git',
+  _git: '🌿 Git',
   _dsp: 'Digital Signal Processing',
 
   _management: 'Management',
